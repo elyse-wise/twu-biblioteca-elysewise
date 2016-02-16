d6ab1e71ceff386b984b92695969e57051530432
elysewise2076 <elyse.wise@gmail.com>
2016-02-16 15:25:03 +1100

    Test Printing one book in a book list

--- src/com/twu/biblioteca/Console.java
+++ src/com/twu/biblioteca/Console.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 import java.io.PrintStream;
+import java.util.List;
 
 /**
  * Created by Elyse on 16/02/2016.
@@ -9,5 +10,9 @@ public class Console {
 
     public void printWelcome(PrintStream out) {
         out.println("Welcome to Biblioteca!");
+    }
+
+    public void printBookList(List<String> books, PrintStream out) {
+        out.println(books.get(0));
     }
 }
--- test/com/twu/biblioteca/ConsoleTest.java
+++ test/com/twu/biblioteca/ConsoleTest.java
@@ -4,9 +4,10 @@ package com.twu.biblioteca;
 import org.junit.Test;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class ConsoleTest {
 
@@ -18,4 +19,18 @@ public class ConsoleTest {
         console.printWelcome(out);
         verify(out).println("Welcome to Biblioteca!");
     }
+
+    @Test
+    public void testOneBookIsPrintedWhenOneBookIsInLibrary() {
+        Console console = new Console();
+        PrintStream out = mock(PrintStream.class);
+
+        List<String> books = new ArrayList<String>();
+        books.add("A Tale of Two Cities");
+
+        console.printBookList(books, out);
+        verify(out, times(1)).println("A Tale of Two Cities");
+    }
+
+
 }
