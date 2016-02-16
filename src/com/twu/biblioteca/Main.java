5e682b642d21762f8a3803db95f9e7408c9cd637
elysewise2076 <elyse.wise@gmail.com>
2016-02-16 22:10:40 +1100

    Refactor main: extract object build methods

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -9,18 +9,22 @@ public class Main {
 
     public static void main(String[] args) {
         Console console = new Console(System.in, System.out);
+        BibliotecaApp application = new BibliotecaApp(console, buildBooks(), buildMenuOptions());
+        application.run();
+    }
 
+    private static Map<String, String> buildMenuOptions() {
+        Map<String, String> menuOptions = new HashMap<String, String>();
+        menuOptions.put("l", "List Books");
+        menuOptions.put("Q", "Quit");
+        return menuOptions;
+    }
+
+    private static List<Book> buildBooks() {
         List<Book> books = new ArrayList<Book>();
         books.add(new Book("Book A", "Author A", "Year A"));
         books.add(new Book("Book B", "Author B", "Year B"));
         books.add(new Book("Book C", "Author C", "Year C"));
-
-        Map<String, String> menuOptions = new HashMap<String, String>();
-        menuOptions.put("l", "List Books");
-        menuOptions.put("Q", "Quit");
-
-
-        BibliotecaApp application = new BibliotecaApp(console, books, menuOptions);
-        application.run();
+        return books;
     }
 }
