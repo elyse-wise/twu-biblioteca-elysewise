eb4403461928c87fd9bf8d7c3955fea5ea85e3b2
elysewise2076 <elyse.wise@gmail.com>
2016-02-16 15:27:50 +1100

    Update Main. Finish List Books story

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -1,9 +1,19 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Console console = new Console();
         console.printWelcome(System.out);
+
+        List<String> books = new ArrayList<String>();
+        books.add("Book A");
+        books.add("Book B");
+        books.add("Book C");
+
+        console.printBookList(books, System.out);
     }
 }
