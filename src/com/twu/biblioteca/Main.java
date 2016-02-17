c8ed994449fa65918be9018ba995c23665faaa57
elysewise2076 <elyse.wise@gmail.com>
2016-02-17 17:28:27 +1100

    Added better examples of book data to main

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Console console = new Console(System.in, System.out);
-        Library library = new Library(buildBooks());
+        Library library = new Library(buildAvailableBooks(), buildCheckedOutBooks());
         BibliotecaApp application = new BibliotecaApp(console, library, buildMenuOperations());
         application.run();
     }
@@ -21,11 +21,22 @@ public class Main {
         return menuOperations;
     }
 
-    private static List<Book> buildBooks() {
+    private static List<Book> buildAvailableBooks() {
         List<Book> books = new ArrayList<Book>();
-        books.add(new Book("Book A", "Author A", "Year A"));
-        books.add(new Book("Book B", "Author B", "Year B"));
-        books.add(new Book("Book C", "Author C", "Year C"));
+        books.add(new Book("A Tale of Two Cities", "Charles Dickens", "1859"));
+        books.add(new Book("The Lord of The Rings", "J.R.R. Tolkein", "1954"));
+        books.add(new Book("The Alchemist", "Paulo Coelho", "1988"));
+        books.add(new Book("The Little Prince", "Antoine De-Saint Exupery", "1943"));
+        books.add(new Book("The Da Vinci Code", "Dan Brown", "2003"));
+        books.add(new Book("Black Beauty", "Anna Sewell", "1877"));
+        return books;
+    }
+
+    private static List<Book> buildCheckedOutBooks() {
+        List<Book> books = new ArrayList<Book>();
+        books.add(new Book("The Hobbit", "J.R.R. Tolkein", "1937"));
+        books.add(new Book("The Lion, The Witch and The Wardrobe", "C.S. Lewis", "1950"));
+        books.add(new Book("Charlotte's Web", "E.B. White", "1952"));
         return books;
     }
 }
