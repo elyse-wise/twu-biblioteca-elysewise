69cdd5eefe1b0b7be68ac0fabe4f4dec18317b23
elysewise2076 <elyse.wise@gmail.com>
2016-02-17 16:29:48 +1100

    Add QuitMenuOperation (which doesn't do anything for now)

---
+++ src/com/twu/biblioteca/QuitMenuOperation.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by Elyse on 17/02/2016.
+ */
+public class QuitMenuOperation extends MenuOperation {
+    public QuitMenuOperation(String trigger, String description) {
+        super(trigger, description);
+    }
+
+    @Override
+    void execute(Library library, Console console) {
+    }
+}
