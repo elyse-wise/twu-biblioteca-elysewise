7a378013f0fc3bc1f5f66a80932b25cd410ebd8b
elysewise2076 <elyse.wise@gmail.com>
2016-02-18 18:33:43 +1100

    Add tests for user logging in

--- test/com/twu/biblioteca/UserLoginMenuOperationTest.java
+++ test/com/twu/biblioteca/UserLoginMenuOperationTest.java
@@ -2,23 +2,91 @@ package com.twu.biblioteca;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.AdditionalAnswers;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Elyse on 18/02/2016.
  */
 public class UserLoginMenuOperationTest {
 
-    UserLoginMenuOperation userMenuLoginOperation;
+    UserLoginMenuOperation userLoginMenuOperation;
+    private Library library;
+    private Console console;
+    private String validLibraryNumber = "123-4567";
 
     @Before
     public void setup() {
-        this.userMenuLoginOperation = new UserLoginMenuOperation("UL", "User Login");
+        this.userLoginMenuOperation = new UserLoginMenuOperation("UL", "User Login");
+        this.library = mock(Library.class);
+        this.console = mock(Console.class);
     }
 
     @Test
-    public void testUserNeedsToBeLoggedIn() {
-        assertFalse(userMenuLoginOperation.needsLogin());
+    public void testUserDoesNotNeedToBeLoggedIn() {
+        assertFalse(userLoginMenuOperation.needsLogin());
     }
+
+    @Test
+    public void testUserIsPromptedForLibraryNumber() {
+        userLoginMenuOperation.execute(library, console);
+        verify(console).printMessage("> Enter library number: ");
+    }
+
+    @Test
+    public void testUserIsPromptedForPasswordIfLibraryNumberEnteredSuccessfully() {
+        String commands[] = {validLibraryNumber};
+        setUserInput(commands);
+
+        userLoginMenuOperation.execute(library, console);
+        verify(console).printMessage("> Enter password: ");
+    }
+
+
+    @Test
+    public void testUserIsNotPromptedForPasswordIfLibraryNumberNotEnteredSuccessfully() {
+        String commands[] = {"*&^%&^%*^%*%*&^"};
+        setUserInput(commands);
+
+        userLoginMenuOperation.execute(library, console);
+        verify(console, never()).printMessage("> Enter password: ");
+    }
+
+    @Test
+    public void testUserIsLoggedInToLibraryWhenTheyEnterAValidLibraryNumberAndValidPassword() {
+        String commands[] = {validLibraryNumber, "validPassword"};
+        setUserInput(commands);
+
+        userLoginMenuOperation.execute(library, console);
+        verify(library).setUserLoggedIn(true);
+    }
+
+    @Test
+    public void testUserIsNotLoggedInToLibraryWhenTheyEnterAValidLibraryNumberAndInvalidPassword() {
+        String commands[] = {validLibraryNumber, "^%$&^%$&$%$^&&%$"};
+        setUserInput(commands);
+
+        userLoginMenuOperation.execute(library, console);
+        verify(library, never()).setUserLoggedIn(true);
+    }
+
+    @Test
+    public void testUserIsNotLoggedInToLibraryWhenTheyEnterAnInValidLibraryNumberAndInvalidPassword() {
+        String commands[] = {"%*&*^%*^*", "*&^%*&^%**%&*"};
+        setUserInput(commands);
+
+        userLoginMenuOperation.execute(library, console);
+        verify(library, never()).setUserLoggedIn(true);
+    }
+
+    private void setUserInput(String inputs[]) {
+        List<String> inputList = Arrays.asList(inputs);
+        when(console.getUserCommand()).thenAnswer(AdditionalAnswers.returnsElementsOf(inputList));
+    }
+
 }
